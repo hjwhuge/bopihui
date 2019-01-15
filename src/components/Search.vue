2c561e89febf6c09b80299e549c81fc8da89c874
Your Name <>
2019-01-15 20:57:53 +0800

    -吗m搜索页修改

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,21 +1,23 @@
 <template>
-    <!-- 头部 -->
-	<mt-header>
-	    <mt-button slot="left">
-	      <svg class="icon" aria-hidden="true">
-	          <use xlink:href="#icon-fanhui"></use>
-	      </svg>
-	    </mt-button>
-	    <mt-button slot="right">
-	        <svg class="icon" aria-hidden="true">
-	            <use xlink:href="#icon-home"></use>
-	        </svg>
-	    </mt-button>
-  	</mt-header>
-  	<!-- 搜索 -->
-	<div class="search">
-	  	
-	</div>
+    <div class="sea">
+    	<!-- 头部 -->
+		<mt-header>
+		    <mt-button slot="left">
+		      <svg class="icon" aria-hidden="true">
+		          <use xlink:href="#icon-fanhui"></use>
+		      </svg>
+		    </mt-button>
+		    <mt-button slot="right">
+		        <svg class="icon" aria-hidden="true">
+		            <use xlink:href="#icon-home"></use>
+		        </svg>
+		    </mt-button>
+	  	</mt-header>
+	  	<!-- 搜索 -->
+		<div class="search">
+		  	
+		</div>
+    </div>
 </template>
 <script>
 export default {
